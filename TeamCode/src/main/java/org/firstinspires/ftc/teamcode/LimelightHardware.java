d077c74fb9058d71a4d94ede88dd9960201480bb
BillyVNYT <billy.gaming2012@gmail.com>
2025-12-30 09:30:55 +0700

    feat: Add LimelightHardware class for AprilTag detection
    Added a new `LimelightHardware` class to interface with the Limelight 3A vision sensor.
    
    This class includes:
    - Initialization of the Limelight hardware.
    - A data class `ApriltagData` to store AprilTag information (x, y, area, id).
    - Methods to get AprilTag data, switch pipelines, and calculate distance to a tag using different methods.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightHardware.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import com.qualcomm.hardware.limelightvision.LLResultTypes.FiducialResult;
+
+import java.util.List;
+
+class ApriltagData{
+    double x;
+    double y;
+    double area;
+    double id;
+    ApriltagData(double x, double y, double area, int id){
+        this.x = x;
+        this.y = y;
+        this.area = area;
+        this.id = id;
+    }
+}
+
+public class LimelightHardware {
+    public Limelight3A limelight;
+    public LimelightHardware(HardwareMap hardwareMap){
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
+        limelight.setPollRateHz(100);
+        limelight.start();
+    }
+
+    public void changePileline(int tab){
+        limelight.pipelineSwitch(tab);
+        limelight.reloadPipeline();
+    }
+    public ApriltagData getAprilTagData(){
+        LLResult result = limelight.getLatestResult();
+
+        if(result != null && result.isValid()){
+            List<FiducialResult> fiducials = result.getFiducialResults();
+            for (FiducialResult fiducial : fiducials){
+                return new ApriltagData(result.getTx(), result.getTy(), result.getTa(), fiducial.getFiducialId());
+            }
+        }
+        return new ApriltagData(0, 0, 0, 0);
+    }
+    public double getDistance(){
+        LLResult result = limelight.getLatestResult();
+
+        if(result != null && result.isValid()){
+            List<FiducialResult> fiducials = result.getFiducialResults();
+            for (FiducialResult fiducial : fiducials){
+                return 179.5511*Math.pow(result.getTa(),-0.6507074);
+            }
+        }
+        return 0;
+    }
+    public double getDistanceByTargetPose(){
+        List<FiducialResult> result = limelight.getLatestResult().getFiducialResults();
+
+        for (FiducialResult fiducial : result) {
+            double x = fiducial.getTargetPoseCameraSpace().getPosition().x;
+            double y = fiducial.getTargetPoseCameraSpace().getPosition().y;
+            double z = fiducial.getTargetPoseCameraSpace().getPosition().z;
+            return Math.sqrt(x*x + y*y + z*z);
+        }
+        return 0;
+    }
+}
