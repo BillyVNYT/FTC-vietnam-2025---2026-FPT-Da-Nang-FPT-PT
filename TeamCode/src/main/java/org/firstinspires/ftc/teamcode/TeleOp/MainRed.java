a5f7b45c6f9f6313bcfba6da873979a08158fed8
BillyVNYT <billy.gaming2012@gmail.com>
2025-12-31 16:31:29 +0700

    feat: Add Outtake class and basic red TeleOp
    Adds a new `Outtake` class to manage the robot's shooting mechanism. This class includes:
    - Initialization for shooter motors and servos.
    - PIDF configuration for the shooter motors.
    - A `Shoot` method that adjusts the shooter angle and velocity based on distance from the Limelight, and then cycles the pre-shoot servo.
    - A moving average filter for smoothing values.
    
    A basic `MainRed` TeleOp has also been created, which initializes the `DriveTrain` and sets up the main opMode loop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainRed.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.DriveTrain;
 
 public class MainRed extends LinearOpMode {
+    private DriveTrain driveTrain = new DriveTrain(hardwareMap);
     @Override
     public void runOpMode() throws InterruptedException {
-
+        waitForStart();
+        while (opModeIsActive()){
+            driveTrain.DrivetrainControlAdvanced(gamepad1, gamepad2);
+        }
     }
 }
