f4c679cc61f15aa7b1af718df88dcc6a2f099b5f
BillyVNYT <billy.gaming2012@gmail.com>
2026-01-02 21:34:00 +0700

    feat: Add manual outtake control and refactor project structure
    This commit introduces a `ManualControl` class to allow for manual control of the outtake turning motor using the gamepad triggers.
    
    Key changes:
    - Created `ManualControl.java` to handle manual outtake motor adjustments.
    - Deleted the `pedroPathing` directory and its contents (`LimelightHardware.java`, `Shooter.java`, `mainRed.java`, `MotorTest.java`).
    - Integrated `ManualControl` into the `MainRed` TeleOp.
    - Added logic to switch between automatic and manual control for the outtake turn, with automatic (`AutoHold`) being the default.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainRed.java
@@ -5,11 +5,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.DriveTrain;
+import org.firstinspires.ftc.teamcode.ManualControl;
 import org.firstinspires.ftc.teamcode.Outtake;
 
 public class MainRed extends LinearOpMode {
     private DriveTrain driveTrain = new DriveTrain(hardwareMap);
     private Outtake outtake = new Outtake(hardwareMap, telemetry);
+    private ManualControl manualControl = new ManualControl(hardwareMap, gamepad1, gamepad2);
     private boolean AutoHold = true;
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,6 +20,14 @@ public class MainRed extends LinearOpMode {
             driveTrain.DrivetrainControlAdvanced(gamepad1, gamepad2);
             if(gamepad1.right_bumper){
                 outtake.Shoot();
+            }
+            if(gamepad1.start){
+                AutoHold = true;
+            }
+            if(AutoHold){
+                outtake.holdOutTake();
+            } else {
+                manualControl.ControlTurnOutTake();
             }
         }
     }
