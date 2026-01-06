1ddfdb6a03b95884341934bfe455a0b23ea5a4cc
Minhdepzai <tranhaiminh14092000@gmail.com>
2026-01-06 21:46:20 +0700

    Con add cái bắn bóng xanh với tím như thế này được không cô?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualControl.java
@@ -10,6 +10,8 @@ import org.firstinspires.ftc.teamcode.SortBall;
 public class ManualControl {
     DcMotor MTurnOuttake;
     DcMotor MOuttakeShooter;
+    DcMotor MShooter1;
+    DcMotor MShooter2;
     DcMotor MIntakeShaft;
     Gamepad Gamepad1;
     Gamepad Gamepad2;
@@ -20,45 +22,85 @@ public class ManualControl {
     };
 
     boolean takeBall = true;
-    public ManualControl(HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2){
+
+    public ManualControl(HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2) {
         MTurnOuttake = hardwareMap.get(DcMotor.class, "MTurnOuttake");
-        MIntakeShaft=hardwareMap.get(DcMotor.class,"MIntakeShaft");
-        MOuttakeShooter=hardwareMap.get(DcMotor.class,"MOuttakeShooter");
+        MIntakeShaft = hardwareMap.get(DcMotor.class, "MIntakeShaft");
+        MOuttakeShooter = hardwareMap.get(DcMotor.class, "MOuttakeShooter");
+        MShooter1 = hardwareMap.get(DcMotor.class, "MShooter1");
+        MShooter2 = hardwareMap.get(DcMotor.class, "MShooter2");
     }
 
-    public void ControlTurnOutTake(){
-        if(Gamepad2.right_trigger >= 0.1){
+    public void ControlTurnOutTake() {
+        if (Gamepad2.right_trigger >= 0.1) {
             MTurnOuttake.setPower(Gamepad2.right_trigger);
-        } else if(Gamepad2.left_trigger >= 0.1){
+        } else if (Gamepad2.left_trigger >= 0.1) {
             MTurnOuttake.setPower(-Gamepad2.left_trigger);
         } else {
             MTurnOuttake.setPower(0);
         }
     }
-    public void ControlIntakeShaft(){
-        if(SortBall.IsFull(load)){
+
+    public void ControlIntakeShaft() {
+        if (SortBall.IsFull(load)) {
             MIntakeShaft.setPower(1);
         }
-        if(Gamepad1.triangle){
+        if (Gamepad1.triangle) {
             takeBall = !takeBall;
-            if(takeBall){
+            if (takeBall) {
                 MIntakeShaft.setPower(1);
-            }
-            else
+            } else
                 MIntakeShaft.setPower(0);
         }
     }
-    public void ControlOuttakeShooter(){
-        if(Gamepad1.cross) {
+
+    public void ControlOuttakeShooter() {
+        if (Gamepad1.cross) {
             while (SortBall.IsFull(load))
                 MOuttakeShooter.setPower(1);
-        }
-
-        else
+        } else
             MOuttakeShooter.setPower(0);
     }
-    public void ShootPurpleArtifact(){
 
+    public void ShootPurpleArtifact() {
+
+        boolean hasPurpleBall = false;
+
+        for (SortBall.BallColor ball : load) {
+            if (ball == SortBall.BallColor.PURPLE) {
+                hasPurpleBall = true;
+                break;
+            }
+        }
+
+        if (Gamepad1.left_bumper && hasPurpleBall) {
+            MShooter1.setPower(1);
+            MShooter2.setPower(1);
+        } else {
+            MShooter1.setPower(0);
+            MShooter2.setPower(0);
+        }
     }
+    public void ShootGreenArtifact() {
 
+        boolean hasGreenBall = false;
+
+        for (SortBall.BallColor ball : load) {
+            if (ball == SortBall.BallColor.GREEN) {
+                hasGreenBall = true;
+                break;
+            }
+        }
+
+        if (Gamepad1.right_bumper && hasGreenBall) {
+            MShooter1.setPower(1);
+            MShooter2.setPower(1);
+        } else {
+            MShooter1.setPower(0);
+            MShooter2.setPower(0);
+        }
+    }
 }
+
+
+
