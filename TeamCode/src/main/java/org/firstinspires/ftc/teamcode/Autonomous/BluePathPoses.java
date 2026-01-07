8ed604658c2788afce7ed7bc17e3ec226f360909
2012thanhtinh-lab <2012thanhtinh@gmail.com>
2026-01-07 19:27:44 +0700

    Update BluePathPoses.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BluePathPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BluePathPoses.java
@@ -6,7 +6,7 @@ public class BluePathPoses {
     public static Pose topZonePose = new Pose(65.17855664951552, 79.94421324671876);
     public static Pose pickupMidPose = new Pose(10.66589629675898, 54.67825604909545);
     public static Pose pickupGatePose = new Pose(11.261, 60.057);
-    public static Pose pickupTopPose = new Pose(13.649, 83.943);
+    public static Pose pickupTopPose = new Pose(18.949289099526066, 83.943);
     public static Pose pickupLowPose = new Pose(8.530805687203792, 36.0);
     public static Pose lowZonePose = new Pose(59.54502369668247, 8.530805687203792);
     public static Pose leavePose= new Pose(21.32701421800948, 69.44075829383885);
