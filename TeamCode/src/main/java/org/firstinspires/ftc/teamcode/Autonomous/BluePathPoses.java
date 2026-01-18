eb170084d2de53f21828008c7fb947dd2c0b7213
2012thanhtinh-lab <2012thanhtinh@gmail.com>
2026-01-18 16:11:58 +0700

    Update pick up gate pose and top zone pose Add pose pickupLowToTopZonePoses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BluePathPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BluePathPoses.java
@@ -3,9 +3,9 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.pedropathing.geometry.Pose;
 
 public class BluePathPoses {
-    public static Pose topZonePose = new Pose(65.17855664951552, 79.94421324671876);
+    public static Pose topZonePose = new Pose(63.469194312796205, 70.63507109004739);
     public static Pose pickupMidPose = new Pose(10.66589629675898, 54.67825604909545);
-    public static Pose pickupGatePose = new Pose(11.261, 60.057);
+    public static Pose pickupGatePose = new Pose(5.32985625834459, 61.00580119771699);
     public static Pose pickupTopPose = new Pose(18.949289099526066, 83.943);
     public static Pose pickupLowPose = new Pose(8.530805687203792, 36.0);
     public static Pose lowZonePose = new Pose(59.54502369668247, 8.530805687203792);
@@ -19,5 +19,6 @@ public class BluePathPoses {
     public static Pose[] pickupTopToTopZonePoses = {pickupTopPose, topZonePose};
     public static Pose[] topZoneToPickupLowPoses = {topZonePose, new Pose(63.299, 25.934), pickupLowPose};
     public static Pose[] pickupLowToLowZonePoses = {pickupLowPose, lowZonePose};
+    public static Pose[] pickupLowToTopZonePoses = {pickupLowPose, new Pose(60, 48), pickupTopPose};
     public static Pose[] lowZoneToLeavePoses = {lowZonePose, leavePose};
 }
