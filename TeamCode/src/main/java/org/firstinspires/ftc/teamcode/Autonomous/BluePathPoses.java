afeec3e3092afddae5921e5c90df9441b8eea022
haphan217 <haphankhanh217@gmail.com>
2026-01-24 11:43:56 +0700

    update blue poses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BluePathPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BluePathPoses.java
@@ -3,10 +3,10 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.pedropathing.geometry.Pose;
 
 public class BluePathPoses {
-    public static Pose topZonePose = new Pose(65.17855664951552, 79.94421324671876);
+    public static Pose topZonePose = new Pose(48.17855664951552, 96.94421324671876);
     public static Pose pickupMidPose = new Pose(9.4, 60.67825604909545);
     public static Pose pickupGatePose = new Pose(3.32985625834459, 64.00580119771699);
-    public static Pose pickupTopPose = new Pose(18.25, 83.943);
+    public static Pose pickupTopPose = new Pose(16.335, 84.133);
     public static Pose pickupLowPose = new Pose(8.030805687203792, 52.0);
     public static Pose lowZonePose = new Pose(59.54502369668247, 8.530805687203792);
     public static Pose leavePose= new Pose(21.32701421800948, 69.44075829383885);
